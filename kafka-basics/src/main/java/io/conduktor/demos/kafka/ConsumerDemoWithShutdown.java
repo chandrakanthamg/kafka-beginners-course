03c670e7492a471ad8f275c1d305b5972f6d05c2
Chandrakantha MG <chandrakanthamg@gmail.com>
2023-02-17 18:52:15 +0530

    Added notes and part1 code

--- kafka-basics/src/main/java/io/conduktor/demos/kafka/ConsumerDemoWithShutdown.java
+++ kafka-basics/src/main/java/io/conduktor/demos/kafka/ConsumerDemoWithShutdown.java
@@ -20,19 +20,19 @@ public class ConsumerDemoWithShutdown {
         log.info("I am a Kafka Consumer!");
 
         String groupId = "my-java-application";
-        String topic = "demo_java";
+        String topic = "third_topic";
 
         // create Producer Properties
         Properties properties = new Properties();
 
         // connect to Localhost
-//        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
+        properties.setProperty("bootstrap.servers", "127.0.0.1:9092");
 
         // connect to Conduktor Playground
-        properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
-        properties.setProperty("security.protocol", "SASL_SSL");
-        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"your-username\" password=\"your-password\";");
-        properties.setProperty("sasl.mechanism", "PLAIN");
+//        properties.setProperty("bootstrap.servers", "cluster.playground.cdkt.io:9092");
+//        properties.setProperty("security.protocol", "SASL_SSL");
+//        properties.setProperty("sasl.jaas.config", "org.apache.kafka.common.security.plain.PlainLoginModule required username=\"your-username\" password=\"your-password\";");
+//        properties.setProperty("sasl.mechanism", "PLAIN");
 
         // create consumer configs
         properties.setProperty("key.deserializer", StringDeserializer.class.getName());
